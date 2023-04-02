5e05bb2d85fec373424e32602b1f61b3f6af81a7
Makar Soloshich <msoloshich@gmail.com>
2023-04-02 11:44:56 +0300

    add binding for fragments

--- app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
@@ -1,18 +1,33 @@
 package com.example.composition.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.composition.R
+import androidx.fragment.app.Fragment
+import com.example.composition.databinding.FragmentChooseLevelBinding
+import com.example.composition.databinding.FragmentWelcomeBinding
+import java.lang.RuntimeException
 
 class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
